8a5fc7d28a2cacba54c8448244adb0c661e6408d
Manuele Menozzi <manuele.menozzi@gmail.com>
2019-03-05 15:13:25 +0100

    Un-skips parsing test with escaped chars in values

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -142,27 +142,32 @@ TXT;
 
     public function testParseFileWithEscapedCharactersInValues()
     {
-        $this->markTestSkipped('To Be Implemented');
         $rows = [];
         Loop::run(function () use (&$rows) {
             $parser = new Parser(yield File\open(__DIR__ . '/countries-with-escaped-charactes-in-value.csv', 'rb'));
             while ($row = yield $parser->parseRow()) {
                 $rows[] = $row;
             }
-            $this->assertCount(4, $rows);
         });
-        $expectedAlgeriaDescription = <<<TXT
-the "People's Democratic" Republic
-of Algeria
-TXT;
-        $this->assertCount(4, $rows);
+
+        $this->assertCount(5, $rows);
         $this->assertEquals(['country', 'description'], $rows[0]);
         $this->assertEquals('Afghanistan', $rows[1][0]);
-        $this->assertEquals('This is the Islamic Republic of Afghanistan', $rows[1][1]);
-        $this->assertEquals('Albania', $rows[1][0]);
-        $this->assertEquals('"This is "the Republic" of Albania', $rows[1][1]);
-        $this->assertEquals('Algeria', $rows[2][0]);
-        $this->assertEquals($expectedAlgeriaDescription, $rows[2][1]);
+        $this->assertEquals('This is the "Islamic Republic" of Afghanistan', $rows[1][1]);
+        $this->assertEquals('Albania', $rows[2][0]);
+        $this->assertEquals('This is "the Republic" of Albania', $rows[2][1]);
+        $this->assertEquals('Algeria', $rows[3][0]);
+        $expectedAlgeria = <<<TXT
+the "People's" Democratic Republic
+of Algeria
+TXT;
+        $this->assertEquals($expectedAlgeria, $rows[3][1]);
+        $this->assertEquals('America', $rows[4][0]);
+        $expectedAmerica = <<<TXT
+the \"People's\"
+of America
+TXT;
+        $this->assertEquals($expectedAmerica, $rows[4][1]);
     }
 
     public function testParseFileWithBom()
